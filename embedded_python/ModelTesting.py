6e5c73e768cc6eba23871173cb6adf26cdd2ff0f
RadGorzy <mdzirg@gmail.com>
2021-05-04 20:47:19 +0200

    Load model only once when classifing specific object (speed up classification)

--- embedded_python/ModelTesting.py
+++ embedded_python/ModelTesting.py
@@ -1,7 +1,7 @@
 import sys
 
 if not hasattr(sys, 'argv'):
-    sys.argv  = ['']
+    sys.argv = ['']
 
 import model as mmodel
 import tensorflow as tf
@@ -20,49 +20,64 @@ _IMG_HEIGHT = 299
       The result of classification (class number to which image belongs)
 
     Raises:
-      
+
     """
-def classify(image,modelPath):
-    model=mmodel.load(modelPath)
-    
+
+
+def classify(image,model):
     image = tf.expand_dims(image, 0)  # Create a batch
+    print("shape = {}".format(image.shape))
     predictions = model.predict(image)
     score = tf.nn.softmax(predictions[0])
-    print("Predictions= {},shape ={}".format(predictions[0],predictions[0].shape))
+    print("Predictions= {},shape ={}".format(predictions[0], predictions[0].shape))
     print("Score= {}".format(score))
     print(
         "This image most likely belongs to {} with a {:.2f} percent confidence."
             .format(np.argmax(score), 100 * np.max(score))
     )
-    return np.argmax(score),predictions[0]
+    return np.argmax(score), predictions[0]
 
-def classify_multiple_projections_and_get_response_vector(projections,modelPath,numOfClasses):
-    score=np.zeros(numOfClasses)
+def classify_multiple_projections_and_get_response_vector(projections, modelPath, numOfClasses):
+    model = mmodel.load(modelPath)
+    score = np.zeros(numOfClasses)
     for image in projections:
-        print("image= {}".format(image))
-        score=score+classify(image,modelPath)[1]
+        # print("image= {}".format(image))
+        score = score + classify(image, model)[1]
 
-    res=score.tolist()
+    res = score.tolist()
     res.insert(0, np.argmax(score))
 
     return tuple(res)
 
-def classify_path(imagePath,modelPath):
 
+def classify_path(imagePath, modelPath):
+    img_array=classify_loadImage(imagePath)
+
+    return classify(img_array, modelPath)
+
+def classify_loadImage(imagePath):
     img = tf.keras.preprocessing.image.load_img(
-        imagePath, target_size=(_IMG_WIDTH, _IMG_HEIGHT),color_mode="grayscale"
+        imagePath, target_size=(_IMG_WIDTH, _IMG_HEIGHT), color_mode="grayscale"
     )
     img_array = tf.keras.preprocessing.image.img_to_array(img)
-    #np.set_printoptions(threshold=sys.maxsize)
-    #print(img_array)
+    # np.set_printoptions(threshold=sys.maxsize)
+    # print(img_array)
     img_array = tf.multiply(tf.cast(img_array, tf.float32), 1.0 / 255.0)
 
-    return classify(img_array,modelPath)
-
+    return img_array
 def classify_test():
-    #res=classify_path("/home/radek/Projects/ImageClassifier/data/images/tree1.jpg","/home/radek/Projects/ImageClassifier/data/models/CNN_binary_Map_person_edited_3x3")
-    res=classify_multiple_projections_and_get_response_vector(["/home/radek/Projects/ImageClassifier/data/images/tree1.jpg","/home/radek/Projects/ImageClassifier/data/images/tree.jpg"],"/home/radek/Projects/ImageClassifier/data/models/CNN_binary_Map_person_edited_3x3",5)
+    # res=classify_path("/home/radek/Projects/ImageClassifier/data/images/tree1.jpg","/home/radek/Projects/ImageClassifier/data/models/CNN_binary_Map_person_edited_3x3")
+    img_batch=[]
+    paths=["/home/radek/Projects/ImageClassifier/data/images/tree1.jpg",
+     "/home/radek/Projects/ImageClassifier/data/images/tree.jpg"]
+    for path in paths:
+        img_arr=classify_loadImage(path)
+        img_batch.append(img_arr)
+
+    res = classify_multiple_projections_and_get_response_vector(img_batch,
+        "/home/radek/Projects/ImageClassifier/data/models/CNN_binary_Map_person_edited_3x3", 5)
     print("CLASS = {}".format(res[0]))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     classify_test()
